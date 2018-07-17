0b2a0718cf39a1d017ca737e0f5cc55bebcf4d81
Divya Satrawada <dsatrawada@gmail.com>
2018-07-17 13:38:26 -0700

    Add Intern Registration UI

---
+++ Internie/Controllers/RegistrationInternViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RegistrationInternViewController.swift
+//  Internie
+//
+//  Created by Divya Satrawada on 7/17/18.
+//  Copyright © 2018 InternFinder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationInternViewController: UIViewController {
+    
+}
