b5ecd43098864ea90a2bd48a8d76a6145d200f5b
Andrew Busel <buselax@gmail.com>
2018-07-18 10:09:12 -0700

    Created View Controllers for Intern Storyboards

---
+++ Internie/Controllers/InternHomePageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Internie
+//
+//  Created by Andrew Busel on 7/18/18.
+//  Copyright © 2018 InternFinder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class InternHomePageViewController: UITableViewController{
+
+}
---
+++ Internie/Controllers/JobPostViewController.swift
@@ -0,0 +1,16 @@
+//
+//  JobPostViewController.swift
+//  Internie
+//
+//  Created by Andrew Busel on 7/18/18.
+//  Copyright © 2018 InternFinder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class JobPostViewController: UIViewController {
+    func setUpViews{
+        fullJobDescriptionView.layer.borderWidth = 1
+        fullJobDescriptionView.layer.borderColor = UIColor.black
+    }
+}
