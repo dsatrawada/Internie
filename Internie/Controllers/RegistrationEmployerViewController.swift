fb085a57449d67ff6498bd3a1eb8c40c848eb864
ktso11 <sokatie11@gmail.com>
2018-07-18 16:52:53 -0700

    working on employerreg field

--- Internie/Controllers/RegistrationEmployerViewController.swift
+++ Internie/Controllers/RegistrationEmployerViewController.swift
@@ -8,7 +8,48 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class RegistrationEmployerViewController: UIViewController {
+    @IBOutlet weak var CreateEmployerButton: UIButton!
+    @IBOutlet weak var EmployerNameField: UITextField!
+    @IBOutlet weak var EmployerEmailField: UITextField!
+    @IBOutlet weak var EmployerAddressField: UITextField!
     
+    @IBOutlet weak var EmployerWebsiteField: UITextField!
+    @IBOutlet weak var EmployerZipCodeField: UITextField!
+    @IBOutlet weak var EmployerStateField: UITextField!
+    @IBOutlet weak var EmployerCityField: UITextField!
+    @IBOutlet weak var EmployerPhoneField: UITextField!
+    @IBAction func CreateEmployerButtonTapped(_ sender: UIButton) {
+        
+        
+        guard let firUser = Auth.auth().currentUser,
+            let username = usernameTextField.text,
+            !username.isEmpty else { return }
+        
+        // 2
+        let userAttrs = ["username": username]
+        
+        // 3
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+        
+        // 4
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return
+            }
+            
+            // 5
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                let user = User(snapshot: snapshot)
+                
+                // handle newly created user here
+            })
+        }
+        
+        
+    }
 }
