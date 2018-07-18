25214045962504da92227dd1f50d2ffd4ae372b6
ktso11 <sokatie11@gmail.com>
2018-07-18 16:24:37 -0700

    all errs fixed

--- Internie/Controllers/InternHomePageViewController.swift
+++ Internie/Controllers/InternHomePageViewController.swift
@@ -14,7 +14,8 @@ class InternHomePageViewController: UITableViewController{
         return 2
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "potentialInternshipCell", for: indexPath)
-        cell.internshipTitleLabel.text = "Job Title for Company Name"
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "potentialInternshipCell", for: indexPath) as! ListPotentialInternshipCell
+        cell.internShipTitleLabel.text = "Job Title for Company Name"
+        return cell
     }
 }
