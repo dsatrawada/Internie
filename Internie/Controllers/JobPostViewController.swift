0b0d294d939eed31289eb4443a94e74e228dc423
Andrew Busel <buselax@gmail.com>
2018-07-18 12:09:13 -0700

    Partially finished job description page for intern viewing

--- Internie/Controllers/JobPostViewController.swift
+++ Internie/Controllers/JobPostViewController.swift
@@ -9,8 +9,24 @@
 import Foundation
 import UIKit
 class JobPostViewController: UIViewController {
-    func setUpViews{
-        fullJobDescriptionView.layer.borderWidth = 1
-        fullJobDescriptionView.layer.borderColor = UIColor.black
+    @IBOutlet weak var jobPostNavigationBar: UINavigationBar!
+    @IBOutlet weak var jobDescriptionView: UIView!
+    @IBOutlet weak var jobDescriptionText: UILabel!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var companyWebsiteLabel: UILabel!
+    @IBOutlet weak var companyAddressLabel: UILabel!
+    @IBOutlet weak var companyCityAndStateLabel: UILabel!
+    @IBOutlet weak var skillRequiredLabel: UILabel!
+    @IBOutlet weak var companyZipLabel: UILabel!
+    func setUpViews(){
+        jobDescriptionView.layer.borderWidth = 1
+        jobDescriptionView.layer.borderColor = UIColor.black
+        jobPostNavigationBar.text = "Job Title"
+        companyNameLabel.text = "Company Name"
+        companyWebsiteLabel.text = "Company Website"
+        companyAddressLabel.text = "Company Address Line 1"
+        skillRequiredLabel.text = "Language Required"
+        jobDescriptionText.text = "Job Bio"
     }
+    
 }
