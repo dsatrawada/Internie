494b88e132a76d21cae4619623ca1b20ddb8937f
Divya Satrawada <dsatrawada@gmail.com>
2018-07-19 18:04:56 -0700

    Add Katie's changes

--- Internie/Controllers/CreateJobPostViewController.swift
+++ Internie/Controllers/CreateJobPostViewController.swift
@@ -8,7 +8,77 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 
 class CreateJobPostViewController: UIViewController{
+    
+    @IBOutlet weak var EmployerJobDescriptionField: UITextView!
+    @IBOutlet weak var CPlusPlusSwitch: UISwitch!
+    @IBOutlet weak var PythonSwitch: UISwitch!
+    @IBOutlet weak var JavaSwitch: UISwitch!
+    @IBOutlet weak var JobPostSaveButton: UIBarButtonItem!
+    @IBOutlet weak var SwiftSwitch: UISwitch!
+    @IBOutlet weak var EmployerJobTitle: UITextField!
+    
+    // index 0: swift
+    // index 1: Java
+    // index 2: python
+    // index 3: C++
+    
+    
+    
+    @IBAction func SaveJobPostTapped(_ sender: UIBarButtonItem) {
+        
+        var skills = [false, false, false, false]
+        
+        guard let firUser = Auth.auth().currentUser,
+            let jobtitle = EmployerJobTitle.text,
+            !jobtitle.isEmpty,
+            let jobdescription = EmployerJobDescriptionField.text,
+            !jobdescription.isEmpty
+            else {
+                print("we have no users")
+                return }
+        
+        
+        if SwiftSwitch.isOn {
+            skills[0] = true
+        }
+        else{
+            skills[0] = false
+        }
+        
+        if JavaSwitch.isOn {
+            skills[1] = true
+        }
+        else{
+            skills[1] = false
+        }
+        
+        if PythonSwitch.isOn {
+            skills[2] = true
+        }
+        else{
+            skills[2] = false
+        }
+        
+        if CPlusPlusSwitch.isOn {
+            skills[3] = true
+        }
+        else{
+            skills[3] = false
+        }
+        
+        
+        
+        JobPostService.create(firUser, jobtitle: jobtitle, jobdescription: jobdescription, skills: skills) { (user) in
+            guard let user = user else { return }
+            
+            
+            print("Created new post")
+        }
+    }
+    
+    
     
 }
---
+++ Internie/Services/JobPostService.swift
@@ -0,0 +1,39 @@
+//
+//  JobPostService.swift
+//  Internie
+//
+//  Created by Katie YK So on 7/19/18.
+//  Copyright © 2018 InternFinder. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+import FirebaseAuth.FIRUser
+
+struct JobPostService {
+    
+    static func create (_ firUser: FIRUser, jobtitle: String, jobdescription: String, skills:
+        [Bool], completion: @escaping (Employer?) -> Void) {
+        
+        let postDict = ["jobtitle": jobtitle,
+                        "jobdescription": jobdescription,
+                        "skills": skills] as [String : Any]
+        
+        let ref = Database.database().reference().child("Post").child(firUser.uid)
+        
+        //writing to the Firebase
+        ref.setValue(postDict) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return completion(nil)
+            }
+        }
+        
+        
+        //get the datasnapshot
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            let user = Employer(snapshot: snapshot)
+            completion(user)
+        })
+    }
+}
